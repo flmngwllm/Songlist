c6f22554395fee9ed636fb2aa6201a871cc9bcc2
William Fleming <chaosmegaman@Williams-MacBook-Pro.local>
2019-06-23 11:52:25 -0400

    created a selected song reducer that passes in a the selectedsong action

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -9,3 +9,10 @@ const songsReducer = () => {
 }
 
 
+const selectedSongsReducer = (selectedSong=null, action) => {
+ if (action.type === 'SONG_SELECTED'){
+     return action.payload;
+ }
+
+ return selectedSong;
+}
